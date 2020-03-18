d005fef0ca4aae6dcea9292d13486ecab74e9c2e
Robert Bohne <robert.bohne@redhat.com>
2020-03-18 21:16:14 +0100

    Some changes for better output of openshift alerts

--- flaskAlert.py
+++ flaskAlert.py
@@ -2,6 +2,8 @@ import telegram
 import logging
 import json
 import os
+import sys
+import pprint
 from flask import Flask
 from flask import request
 from flask_basicauth import BasicAuth
@@ -20,11 +22,12 @@ chatID = os.environ['TELEGRAM_CHATID']
 def postAlertmanager():
 
     content = json.loads(request.get_data())
+    pprint.pprint(content)
     with open("Output.txt", "w") as text_file:
         text_file.write("{0}".format(content))
     try:
         for alert in content['alerts']:
-            message = """Status: """+alert['status']+""" \n"""
+            message = "Status: %s\n" % alert['status']
             message += """Alertname: """+alert['labels']['alertname']+""" \n"""
 
             if alert['status'] == "firing":
@@ -33,17 +36,26 @@ def postAlertmanager():
             if alert['status'] == "resolved":
                 message += """Resolved: """+alert['endsAt']+""" \n"""
 
-            if 'name' in alert['labels']:
-                message += """Instance: """+alert['labels']['instance']+"""("""+alert['labels']['name']+""") \n"""
-            else:
-                message += """Instance: """+alert['labels']['instance']+""" \n"""
+            if  "labels" in alert:
+                message += """Labels:\n"""
+                for label in alert['labels']:
+                    message += "\t %s :  %s\n" % (label,alert['labels'][label])
+            
+            if "generatorURL" in alert:
+                message += "%s\n" %  alert['generatorURL']
 
-            message += """\n\n"""+alert['annotations']['description']+""""""
+            if "annotations" in alert:
+                if "message" in alert['annotations']:
+                    message += "\n%s\n" % alert['annotations']['message']
+
 
             bot.sendMessage(chat_id=chatID, text=message)
             return "Alert OK", 200
+    except KeyError as error:
+        bot.sendMessage(chat_id=chatID, text="Error! " + str(error)   )
+        return "Alert nOK", 200
     except:
-        bot.sendMessage(chat_id=chatID, text="Error! with content: " + content )
+        bot.sendMessage(chat_id=chatID, text="Error! with content: " + str(sys.exc_info()[0]) )
         return "Alert nOK", 200
 
 
